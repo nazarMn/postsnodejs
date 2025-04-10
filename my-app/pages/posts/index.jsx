6075749bb2198564ab5896bce2370e34e2a26b7f
MarkivNazar <nazarmn2008@gmail.com>
2025-04-10 18:21:27 +0300

    add style posts

--- my-app/pages/posts/index.jsx
+++ my-app/pages/posts/index.jsx
@@ -3,18 +3,27 @@ import Link from "next/link";
 
 export default function PostsList({ posts }) {
     return (
-        <div>
-            <h1 className="text-3xl font-bold">Posts</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link href={`/posts/${post.id}`}>
-                            {post.title}
-                        </Link>
-                    </li>   
-                ))}
-            </ul>
-        </div>
+        <div className=" mx-auto p-6 bg-white shadow-lg rounded-2xl">
+    <h1 className="text-3xl font-bold text-gray-800 mb-6">Posts</h1>
+    <ul className="space-y-4">
+        {posts.map((post) => (
+            <li
+                key={post.id}
+                className="bg-gray-100 hover:bg-gray-200 transition-colors duration-300 rounded-xl p-4 justify-between flex items-center"
+            >
+              
+                    {post.title}
+                    <Link
+                    href={`/posts/${post.id}`}
+                    className="text-xl text-blue-500 color-foreground hover:underline"
+                >
+                    Read more
+                </Link>
+            </li>
+        ))}
+    </ul>
+</div>
+
     );
 }
 
